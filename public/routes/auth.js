ae5cbfd88a92ea178484785548a54e78e921bf89
Zain-appiness <zain@appinessworld.com>
2024-12-10 16:10:14 +0530

    ADDING CRUD API'S

--- public/routes/auth.js
+++ public/routes/auth.js
@@ -39,4 +39,134 @@ router.post('/login', async (req, res) => {
   }
 });
 
+router.get('/user', async(req,res)=>{
+    try{
+      const users = await User.findAll();
+
+      if (!users || users.length === 0) {
+        return res.status(400).json({ message: 'NO USERS EXIST' });
+      }
+  
+      res.status(200).json({
+        message: 'ALL THE USERS',
+        users,
+      });
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+
+});
+
+router.get('/user/:id',async (req,res)=>{
+  const userId=req.params.id;
+  try {
+    const user= await User.findOne({
+      where:{id: userId},
+    });
+    
+ 
+    if (!user || user.length === 0) {
+      return res.status(400).json({ message: 'NO USERS EXIST' });
+    }
+
+    res.status(200).json({
+      message: 'ALL THE USERS',
+      user,
+    });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+
+});
+
+router.get('/user/email/:email',async(req,res)=>{
+  const userEmail= req.params.email;
+  try {
+    const user= await User.findOne({
+      where:{
+        email: userEmail,
+      }
+    });
+    if(!user|| user.length==0){
+      res.status(400).json({
+        message:'NO SUCH USER'
+      });
+    }
+
+    res.status(200).json({
+      message:'USER FOUND:',
+      user
+    })
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+router.put('/update/:id',async(req,res)=>{
+  const userId= req.params.id;
+  const userData= req.body;
+  try {
+    if(!userData|| userData.length==0){
+      res.status(400).json({
+        message:'NO UPDATE DATA PROVIDED'
+      });
+    }
+
+    const user= await User.findByPk(userId);
+
+    if(!user){
+      if(!user|| user.length==0){
+        res.status(400).json({
+          message:'NO SUCH USER'
+        });
+      }
+    }
+
+    await user.update(userData);
+
+    res.status(200).json({
+      message: 'User updated successfully',
+      user, 
+    });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+
+router.delete('/delete/:id',async(req,res)=>{
+  const userID= req.params.id;
+  try {
+      const user= await User.findOne({
+        where:{
+          id: userID,
+        }
+      });
+
+      if(!user){
+        if(!user|| user.length==0){
+          res.status(400).json({
+            message:'NO SUCH USER'
+          });
+        }
+      }
+
+      const{email}= user;
+
+      await User.destroy({
+        where:{
+          email: email,
+        }
+      })
+      res.status(200).json({
+        message: 'User delted successfully',
+        user, 
+      });
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  });
+  
+
+
 module.exports = router;
